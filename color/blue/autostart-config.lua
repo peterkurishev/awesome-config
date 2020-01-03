62a3ca99e6fd75266edaefe4ad3541f216a0c7a6
Peter A. Kurishev <peter.kurishev@gmail.com>
2020-01-04 01:33:39 +0300

    Autostart commands at awesome

--- color/blue/autostart-config.lua
+++ color/blue/autostart-config.lua
@@ -11,26 +11,41 @@ local autostart = {}
 
 -- Application list function
 --------------------------------------------------------------------------------
+-- startup programs
+
 function autostart.run()
 	-- environment
-	awful.spawn.with_shell("python ~/scripts/env/pa-setup.py")
-	awful.spawn.with_shell("python ~/scripts/env/color-profile-setup.py")
-	awful.spawn.with_shell("python ~/scripts/env/kbd-setup.py")
+	-- awful.spawn.with_shell("python ~/scripts/env/pa-setup.py")
+	-- awful.spawn.with_shell("python ~/scripts/env/color-profile-setup.py")
+	-- awful.spawn.with_shell("python ~/scripts/env/kbd-setup.py")
 
-	-- gnome environment
-	awful.spawn.with_shell("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+	-- -- gnome environment
+	-- awful.spawn.with_shell("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
 
-	-- firefox sync
-	awful.spawn.with_shell("python ~/scripts/firefox/ff-sync.py")
+	-- -- firefox sync
+	-- awful.spawn.with_shell("python ~/scripts/firefox/ff-sync.py")
 
 	-- utils
-	awful.spawn.with_shell("compton")
+	--awful.spawn.with_shell("compton")
+
 	awful.spawn.with_shell("nm-applet")
 
 	-- apps
 	awful.spawn.with_shell("gpaste-client start")
-	awful.spawn.with_shell("transmission-gtk -m")
-	awful.spawn.with_shell("pragha --toggle_view")
+	--awful.spawn.with_shell("transmission-gtk -m")
+	--awful.spawn.with_shell("pragha --toggle_view")
+  -- awful.spawn.with_shell("telegram-desktop", {
+  --                          floating  = true,
+  --                          tag       = mouse.screen.selected_tag,
+  --                          placement = awful.placement.bottom_right,
+  -- })
+  --awful.spawn.with_shell("skypeforlinux")
+  --spawn_once("skypeforlinux", "Skype", screen[1].tags[5])
+
+  awful.spawn.with_shell("firefox", {tag=root.tags()[4]})
+  --spawn_once("firefox", "Firefox", screen[1].tags[2])
+  
+  awful.spawn.with_shell("kitty", "Term")
 end
 
 -- Read and commads from file and spawn them
